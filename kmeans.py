6b996bd8d7c584ce45963e6c0b3e1d5171bf3f7e
samTheDeveloper <morakinyodamilare6@gmail.com>
2023-03-14 15:17:07 -0700

    First commit

---
+++ kmeans.py
@@ -0,0 +1,26 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import scale
+import pandas as pd
+
+bc = load_breast_cancer()
+#print(bc)
+x = scale(bc.data)
+#print(x) 
+y = bc.target
+print(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=6)
+model = KMeans(n_clusters=2, random_state=0)
+model.fit(x_train)
+predictions = model.predict(x_test)
+labels = model.labels_
+accuracy = accuracy_score(y_test, predictions)
+
+print('labels:', labels)
+print('predictions:', predictions)
+print('accuracy:',accuracy)
+print('actual:',y_test)
+print(pd.crosstab(y_train, labels))
---
+++ melbourne.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from sklearn. model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+from sklearn import datasets
+import matplotlib.pyplot as plt
+
+datasets = pd.read_csv('insurance.csv')
+#print(datasets.head())
+#sex
+label = LabelEncoder()
+label.fit(datasets.sex.drop_duplicates())
+datasets.sex = label.transform(datasets.sex)
+
+#smoker
+label.fit(datasets.smoker.drop_duplicates())
+datasets.smoker = label.transform(datasets.smoker)
+
+label.fit(datasets.region.drop_duplicates())
+datasets.region = label.transform(datasets.region)
+
+print(datasets.head())
+
+#creating our mode
+x_lin = datasets.drop(['charges'], axis=1)
+y_lin = datasets[['charges']]
+
+x_lin_train, x_lin_test, y_lin_train, y_lin_test = train_test_split(x_lin, y_lin, test_size=0.2, random_state=42)
+linear_model = LinearRegression()
+
+linear_model.fit(x_lin, y_lin)
+for idx, col_name in enumerate(x_lin_train.columns):
+    print('the coefficient for {} is {}'.format(col_name, linear_model.coef_[0][idx]))
