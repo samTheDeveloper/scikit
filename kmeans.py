e2097fb72e2556f8e16745e6c33efdd3d9dd4db0
samTheDeveloper <125812584+samTheDeveloper@users.noreply.github.com>
2023-03-18 13:10:30 -0700

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import scale
 import pandas as pd
-from sklearn import datasets
+
 
 bc = load_breast_cancer()
 print(bc)
@@ -13,6 +13,7 @@ print(x)
 y = bc.target
 print(y)
 
+#training the model
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=6)
 model = KMeans(n_clusters=2, random_state=0)
 model.fit(x_train)
