06efe1a9dfa27097cc75f83ab1eec3dc5335d348
Neural_byte <125812584+samTheDeveloper@users.noreply.github.com>
2023-04-15 09:01:52 -0400

    Update melbourne.py

--- melbourne.py
+++ melbourne.py
@@ -8,6 +8,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
+
 datasets = pd.read_csv('insurance.csv')
 #print(datasets.head())
 #sex
